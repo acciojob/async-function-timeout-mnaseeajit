495c2ca8b9379696eaceb509a5793d4101a438a0
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-23 13:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+
+
+
+document.getElementById("btn").addEventListener("click", async function () {
+    let text = document.getElementById("text").value;
+    let delay = document.getElementById("delay").value;
+
+    if (text && delay) {
+        let myPromise = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(text);
+            }, delay);
+        });
+
+        let text2 = await myPromise;
+
+        if (text2) {
+            document.getElementById("output").textContent = text2; // Use textContent instead of innerContent
+        }
+    }
+});
+
